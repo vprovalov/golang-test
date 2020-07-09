e14efea971df9f22e22f5a8bf43107c217442ca8
Valeriy Provalov <valeriy.provalov@gmail.com>
2020-07-08 22:21:53 -0700

    hello world web server

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Printf("Hello, world!")
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, world!")
 }
